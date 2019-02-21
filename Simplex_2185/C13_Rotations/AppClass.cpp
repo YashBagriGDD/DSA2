fa5ddc1c94b6bad28c203e9551ad9f7a2046c065
Yash Bagri <ybagri@gmail.com>
2019-02-21 15:12:13 -0500

    Quaternions

--- Simplex_2185/C13_Rotations/AppClass.cpp
+++ Simplex_2185/C13_Rotations/AppClass.cpp
@@ -27,17 +27,28 @@ void Application::Display(void)
 
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix(); //view Matrix
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix(); //Projection Matrix
-	
+	matrix4 m4Model = IDENTITY_M4;
+
 	//Get a timer
 	static uint uClock = m_pSystem->GenClock();
 	float fTimer = m_pSystem->GetTimeSinceStart(uClock);
+	matrix4 m4Translation = glm::translate(vector3(1.0f, 0.0f,0.0f));
 
+	glm::quat q1 = glm::angleAxis(glm::radians(0.0f), AXIS_Z);
+	quaternion q2 = glm::angleAxis(glm::radians(90.0f), AXIS_Z);
+
+	float fTotalTime = 3.75f;
+	float fPercentage = MapValue(fTimer, 0.0f, fTotalTime, 0.0f, 1.0f);
+	quaternion q3 = glm::mix(q1, q2, fPercentage);
+
+
+	/*
 	//calculate the current position
 	matrix4 m4Rotation = glm::rotate(IDENTITY_M4, glm::radians(fTimer * 60.0f), vector3(0.0f, 0.0f, 1.0f));
 	matrix4 m4Model;
 	for (uint i = 0; i < 2500; ++i)
 		m4Model = m4Rotation * glm::translate(IDENTITY_M4, vector3(2.5f, 0.0f, 0.0f)) * glm::transpose(m4Rotation);
-	
+	*/
 	/*
 	//extra part, how to rotate around a point (in this case the base of the cone)
 	matrix4 m4Translation = glm::translate(IDENTITY_M4, vector3(0.0f, 0.5f, 0.0f));
@@ -46,7 +57,7 @@ void Application::Display(void)
 	*/
 
 	// render the object
-	m_pMesh->Render(m4Projection, m4View, m4Model);
+	m_pMesh->Render(m4Projection, m4View, ToMatrix4(q3));
 	
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
