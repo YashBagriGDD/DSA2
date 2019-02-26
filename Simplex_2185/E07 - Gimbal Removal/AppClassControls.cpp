c27ebd80ecc19a3030ba76adefa2fc186b7cefda
Yash Bagri <ybagri@gmail.com>
2019-02-26 14:16:46 -0500

    Gimbal Lock done

--- Simplex_2185/E07 - Gimbal Removal/AppClassControls.cpp
+++ Simplex_2185/E07 - Gimbal Removal/AppClassControls.cpp
@@ -417,23 +417,24 @@ void Application::ProcessKeyboard(void)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
 	{
 		if (fMultiplier)
-			m_v3Rotation.x -= 1.0f;
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), AXIS_X);
+			//m_v3Rotation.x -= 1.0f;
 		else
-			m_v3Rotation.x += 1.0f;
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(-1.0f), AXIS_X);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y))
 	{
 		if (fMultiplier)
-			m_v3Rotation.y -= 1.0f;
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), AXIS_Y);
 		else
-			m_v3Rotation.y += 1.0f;
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(-1.0f), AXIS_Y);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
 	{
 		if (fMultiplier)
-			m_v3Rotation.z -= 1.0f;
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), AXIS_Z);
 		else
-			m_v3Rotation.z += 1.0f;
+			m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(-1.0f), AXIS_Z);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::R))
 	{
